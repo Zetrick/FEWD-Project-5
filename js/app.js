8a6c2fb66e96e130f8b07fbac022ebbf9c544cf8
Zach Hetrick <Zetrick@gmail.com>
2021-10-25 14:45:54 -0800

    Small changes
    added padding to gallery div. Fixed Responsive Design Mode in FireFox not showing arrow buttons when viewing photos.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-baguetteBox.run( '.gallery' );
+baguetteBox.run( '.gallery', {
+    //This forces buttons to be visible in Responsive Design Mode in FireFox
+    //Otherwise they aren't there...
+    "buttons": true
+});
